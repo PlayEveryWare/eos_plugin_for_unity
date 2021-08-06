4f9d893417fb7f0c8764626f1d0d34a7d649bd6d
andrew <ahirata@playeveryware.com>
2021-08-05 21:22:03 -0700

    chore: add correct annotations to memory methods; create delegates

--- Assets/Plugins/Source/SystemMemory.cs
+++ Assets/Plugins/Source/SystemMemory.cs
@@ -20,7 +20,7 @@
 * SOFTWARE.
 */
 
-﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 using System.Reflection;
 using System;
 
@@ -28,23 +28,30 @@ using size_t = System.UIntPtr;
 
 public partial class SystemMemory
 {
-#if !(UNITY_ANDROID || UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA_10_0)
+#if !(UNITY_ANDROID || UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA_10_0) || UNITY_SWITCH
+    public delegate IntPtr EOS_GenericAlignAlloc(size_t sizeInBytes, size_t alignmentInBytes);
+    public delegate IntPtr EOS_GenericAlignRealloc(IntPtr ptr, size_t sizeInBytes, size_t alignmentInBytes);
+    public delegate void EOS_GenericFree(IntPtr ptr);
+
+    [AOT.MonoPInvokeCallback(typeof(EOS_GenericAlignAlloc))]
     static public IntPtr GenericAlignAlloc(size_t sizeInBytes, size_t alignmentInBytes)
     {
         return Mem_generic_align_alloc(sizeInBytes, alignmentInBytes);
     }
 
+    //-------------------------------------------------------------------------
+    [AOT.MonoPInvokeCallback(typeof(EOS_GenericAlignRealloc))]
     static public IntPtr GenericAlignRealloc(IntPtr ptr, size_t sizeInBytes, size_t alignmentInBytes)
     {
         return Mem_generic_align_realloc(ptr, sizeInBytes, alignmentInBytes);
     }
 
+    //-------------------------------------------------------------------------
+    [AOT.MonoPInvokeCallback(typeof(EOS_GenericFree))]
     static public void GenericFree(IntPtr ptr)
     {
         Mem_generic_free(ptr);
     }
-
-
 
     [DllImport("__Internal")]
     static public extern IntPtr Mem_generic_align_alloc(size_t size_in_bytes, size_t alignment_in_bytes);
