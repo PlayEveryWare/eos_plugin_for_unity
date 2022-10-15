36e4774056e3519b56d6cf7605fd2441f7ebd76b
arthur272 <arthur740212@gmail.com>
2022-10-14 17:48:42 -0700

    fix(mac) : hack on macOS native to obtain correct mic permission status
    Mac standalone build couldn't grab the current mic status, instead it grabs the status at program startup.
    This hack pops the permission request preemptively, and stores that result.

--- Assets/Scripts/UI/UIMemberEntry.cs
+++ Assets/Scripts/UI/UIMemberEntry.cs
@@ -192,13 +192,14 @@ namespace PlayEveryWare.EpicOnlineServices.Samples
                 Debug.LogError("MemberEntryPromoteButtonOnClick: PromoteOnClick action is null!");
             }
         }
-#if (UNITY_IOS  && EOS_PREVIEW_PLATFORM) //|| (UNITY_STANDALONE_OSX && EOS_PREVIEW_PLATFORM)
-        [System.Runtime.InteropServices.DllImport("__Internal")]
+
+#if (UNITY_IOS || UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX) && EOS_PREVIEW_PLATFORM
+        [System.Runtime.InteropServices.DllImport("MicrophoneUtility_macos.dylib")]
         public static extern bool MicrophoneUtility_get_mic_permission();
 #endif
         private bool HasPlatformMicrophonePermission()
         {
-#if (UNITY_IOS && EOS_PREVIEW_PLATFORM) //|| (UNITY_STANDALONE_OSX && EOS_PREVIEW_PLATFORM)
+#if (UNITY_IOS || UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX) && EOS_PREVIEW_PLATFORM
             return MicrophoneUtility_get_mic_permission();
 #elif UNITY_ANDROID && EOS_PREVIEW_PLATFORM
             return UnityEngine.Android.Permission.HasUserAuthorizedPermission(UnityEngine.Android.Permission.Microphone);
