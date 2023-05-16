98d5ad58d94d3a4dbed7b029e1bd1dd218595bc3
arthur740212 <36757173+arthur740212@users.noreply.github.com>
2023-05-16 11:09:11 -0700

    Friends tab collapse/expand fix (#273)
    * fix(UI) : friends UI action
    
    * fix(UI) : Fix the wrong unsubscribing functions and add null check for actions

---
+++ Assets/Scripts/UI/UIActions.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class UIActions
+{
+    public static Action OnCollapseFriendsTab;
+    public static Action OnExpandFriendsTab;
+}
--- Assets/Scripts/UI/UILobbiesMenu.cs
+++ Assets/Scripts/UI/UILobbiesMenu.cs
@@ -69,6 +69,7 @@ namespace PlayEveryWare.EpicOnlineServices.Samples
         [Header("Lobbies UI - Search")]
         public GameObject UILobbyEntryPrefab;
         public GameObject SearchContentParent;
+        public GameObject LobbySearchUI;
 
         public UIConsoleInputField SearchByBucketIdBox;
         public UIConsoleInputField SearchByLevelBox;
@@ -104,6 +105,8 @@ namespace PlayEveryWare.EpicOnlineServices.Samples
 
         public void Awake()
         {
+            UIActions.OnCollapseFriendsTab += EnableInterferingUIForFriendsTab;
+            UIActions.OnExpandFriendsTab += DisableInterferingUIForFriendsTab;
             // Hide Invite Pop-up (Default)
             UIInvitePanel.SetActive(false);
 
@@ -143,8 +146,21 @@ namespace PlayEveryWare.EpicOnlineServices.Samples
             ClearSearchResults();
         }
 
+        private void DisableInterferingUIForFriendsTab()
+        {
+            LobbySearchUI.SetActive(false);
+        }
+
+        private void EnableInterferingUIForFriendsTab()
+        {
+            LobbySearchUI.SetActive(true);
+        }
+
         private void OnDestroy()
         {
+            UIActions.OnCollapseFriendsTab -= EnableInterferingUIForFriendsTab;
+            UIActions.OnExpandFriendsTab -= DisableInterferingUIForFriendsTab;
+
             LobbyManager?.RemoveNotifyMemberUpdate(OnMemberUpdate);
 
             EOSManager.Instance.RemoveManager<EOSLobbyManager>();
@@ -782,7 +798,7 @@ namespace PlayEveryWare.EpicOnlineServices.Samples
                 }
             }
         }
-
+     
         public void ShowMenu()
         {
             EOSManager.Instance.GetOrCreateManager<EOSLobbyManager>().OnLoggedIn();
