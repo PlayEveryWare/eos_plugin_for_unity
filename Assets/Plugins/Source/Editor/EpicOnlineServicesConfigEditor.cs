26187bdf28c22a706e2372ad6aead03946b0df99
arthur272 <arthur740212@gmail.com>
2023-08-09 21:59:20 -0700

    fix(config,steam): compile errors on platforms that don't support steamworks
    This commit fixes an error happening in the Steam config tool.
    
    This is likely an overkill with the refactor however.
    
    The easier fix is to just add the whole line of platform define for DISABLESTEAMWORKS into EpicOnlineServicesConfigEditor.cs
    This is efficient if a small number of scripts uses steamworks.
    If it grows the same define would need to be added all the scripts

--- Assets/Plugins/Source/Editor/EpicOnlineServicesConfigEditor.cs
+++ Assets/Plugins/Source/Editor/EpicOnlineServicesConfigEditor.cs
@@ -27,7 +27,6 @@ using System.IO;
 using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
-using PlayEveryWare.EpicOnlineServices;
 using System.Collections.Generic;
 
 namespace PlayEveryWare.EpicOnlineServices
@@ -616,10 +615,10 @@ _WIN32 || _WIN64
             AssigningTextField("Override Library path", ref steamEOSConfigFile.currentEOSConfig.overrideLibraryPath);
             AssigningUintField("Steamworks SDK major version", ref steamEOSConfigFile.currentEOSConfig.steamSDKMajorVersion, 190);
             AssigningUintField("Steamworks SDK minor version", ref steamEOSConfigFile.currentEOSConfig.steamSDKMinorVersion, 190);
-#if STEAMWORKS_MODULE && !DISABLESTEAMWORKS
+
             if (GUILayout.Button("Update from Steamworks.NET", GUILayout.MaxWidth(200)))
             {
-                var steamworksVersion = Steamworks.Version.SteamworksSDKVersion;
+                var steamworksVersion = Steamworks_Utility.GetSteamworksVersion();
                 var versionParts = steamworksVersion.Split(".");
                 bool success = false;
                 if (versionParts.Length >= 2)
@@ -637,7 +636,6 @@ _WIN32 || _WIN64
                     Debug.LogError("Failed to retrive Steamworks SDK version from Steamworks.NET");
                 }
             }
-#endif
         }
 
         // TODO: create way to hook up new platforms dynamically 
