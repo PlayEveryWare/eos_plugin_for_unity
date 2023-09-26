0008fb28a9e0d5b17b72892818c68dd0917f946c
Paul Hazen <paul.hazen@playeveryware.com>
2023-09-25 17:58:31 -0700

    Updated powershell setup script.

---
+++ scripts/etc/install-winget.ps1
@@ -0,0 +1,9 @@
+# Install winget
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading WinGet and its dependencies..."
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
--- scripts/setup-windows.ps1
+++ scripts/setup-windows.ps1
@@ -1,3 +1,9 @@
+# Before running this script, you will need to enter the following command:
+# Set-RemoteExecutionPolicy RemoteSigned -Force
+
+# Install winget
+. ./etc/install-winget.ps1
+
 # Install git
 winget install --id Git.Git -e --source winget
 # Install unity hub
