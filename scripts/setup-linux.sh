7f45078e016a537fd5fed1c93d5bcac4cdb2a391
Paul Hazen <paul.hazen@playeveryware.com>
2023-09-26 15:56:48 -0700

    Updated add-apt-repository command to auto confirm.

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -4,7 +4,7 @@
 # TODO: Test this script on a fresh install of 18.04 (or whatever is currently supported)
 
 # Add source for git 
-sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:git-core/ppa -y
 # Add source for unity hub
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
