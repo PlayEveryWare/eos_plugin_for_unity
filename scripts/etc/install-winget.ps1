4f0e41b2e5f97a9b0f4f5f5c4fec82b502a41afe
Paul Hazen <paul-hazen@live.com>
2023-09-25 21:32:34 -0700

    Removed xaml installation script.

--- scripts/etc/install-winget.ps1
+++ scripts/etc/install-winget.ps1
@@ -1,16 +1,10 @@
-#. $PSScriptRoot/install-xaml.ps1
 $progressPreference = 'silentlyContinue'
 Write-Information "Downloading WinGet and its dependencies..."
 
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile $PSScriptRoot/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile $PSScriptRoot/Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile $PSScript/Microsoft.UI.Xaml.2.7.x64.appx
 
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-
-
-Remove-Item Microsoft.VCLibs.x64.14.00.Desktop.appx
-Remove-Item Microsoft.UI.Xaml.2.7.x64.appx
-Remove-Item Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Add-AppxPackage $PSScriptRoot/Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage $PSScriptRoot/Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage $PSScriptRoot/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
