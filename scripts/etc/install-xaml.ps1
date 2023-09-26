da5e09a217b1d94a4ab8a759de0f05ff00726cf4
Paul Hazen <paul-hazen@live.com>
2023-09-25 20:33:15 -0700

    Changed version of xaml installed to 2.7

--- scripts/etc/install-xaml.ps1
+++ scripts/etc/install-xaml.ps1
@@ -1,12 +1,14 @@
-﻿# Install prerequisite to winget
+﻿Write-Host "Installing Xaml package"
+
+# Install prerequisite to winget
 Invoke-WebRequest `
     -UseBasicParsing `
-    -Uri "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.8.5" `
+    -Uri "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.0" `
     -OutFile Xaml.zip
 
 Expand-Archive Xaml.zip
 
-Add-AppxPackage .\Xaml\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.8.appx
+Add-AppxPackage .\Xaml\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx
 
 Remove-Item Xaml.zip
 Remove-Item -Recurse -Force Xaml
