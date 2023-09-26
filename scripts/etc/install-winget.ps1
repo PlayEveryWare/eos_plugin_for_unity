1019cf6e983491254b37259f3d9c1f6b7b9c0485
Paul Hazen <paul-hazen@live.com>
2023-09-25 18:18:33 -0700

    Changed install winget script.

--- scripts/etc/install-winget.ps1
+++ scripts/etc/install-winget.ps1
@@ -1,16 +1,4 @@
 # Install winget
-$progressPreference = 'silentlyContinue'
-Write-Information "Downloading WinGet and its dependencies..."
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
-
-Write-Host "Installing packages for winget"
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx --accept-source-agreements
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx --accept-source-agreements
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle --accept-source-agreements
-
-# Remove the downloaded files after they've been added.
-Remove-Item Microsoft.VCLibs.x64.14.00.Desktop.appx
-Remove-Item Microsoft.UI.Xaml.2.7.x64.appx
-Remove-Item Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Install-Module -Name WingetTools
+Install-WinGet
+winget upgrade --all --silent --accept-package-agreements --accept-source-agreements --force
