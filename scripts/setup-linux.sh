4f3af4b6ce3188edada10b9a90d1a760490242a6
Paul Hazen <paul.hazen@playeveryware.com>
2023-09-26 16:34:19 -0700

    Added commands to install the editor, get a license, and build the project.

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -19,4 +19,21 @@ sudo apt install build-essential git unityhub xvfb -y
 # Install Unity 2021.3 (for some reason cannot specify 2021.3.8f1)
 # NOTE: The string below will need to be updated when the project is upgraded
 # to support newer versions of the editor
-sudo unityhub --headless install --version 2021.3 --no-sandbox
+# sudo unityhub --headless install --version 2021.3 --no-sandbox
+
+sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
+sudo apt update
+sudo apt install unityhub
+sudo apt install libgbm-dev libasound2 libgconf-2-4 xvfb
+
+UNITY_VERSION=2021.3.30f1
+
+sudo xvfb-run unityhub --headless install --version $UNITY_VERSION --no-sandbox
+
+# Generate a license activation file (.alf)
+sudo xvfb-run Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity -quit -batchmode -nographics -createManualActivationFile
+# Get the license
+sudo xvfb-run Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity -quit -batchmode -nographics -manualLicenseFile yourLicenseFile.ulf
+
+sudo xvfb-run Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity -quit -batchmode -nographics -logFile - -projectPath ../ -buildTarget Linux64 -buildLinux64Player "Build.x86_64"
