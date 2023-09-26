13b9f1638eb86448ab9e7f4307b16dd3e92ccc31
Paul Hazen <paul.hazen@playeveryware.com>
2023-09-25 18:34:23 -0700

    installing unity editor.

--- scripts/setup-windows.ps1
+++ scripts/setup-windows.ps1
@@ -2,12 +2,28 @@
 # Set-RemoteExecutionPolicy RemoteSigned -Force
 
 # Install winget
-. ./etc/install-winget.ps1
+. $PSScriptRoot/etc/install-winget.ps1
 
 # Install git
 winget install --id Git.Git -e --source winget
+
 # Install unity hub
 winget install -e Unity.UnityHub
+
+# Get the version of the unity editor to install
+$unityEditorVersion = Get-Content $PSScriptRoot/../ProjectSettings/ProjectVersion.txt | 
+    Where-Object { $_ -imatch 'm_EditorVersion' } |
+    ForEach-Object { $_ -ireplace 'm_EditorVersion:\ '} |
+    Select -First 1
+
+winget install ("Unity.Unity.{0}" -f $unityEditorVersion.Substring(4)) -v $unityEditorVersion
+
+Read-Host
+
+# Install unity editor version supported by the plugin
+#winget install Unity.Unity.2021 -v 2021.3.8f1
+
+
 
 
 
