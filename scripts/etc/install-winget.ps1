88f850a690fb19d9a3561f38a78bb36b1c7aa3f0
Paul Hazen <paul.hazen@playeveryware.com>
2023-09-26 14:47:44 -0700

    Added setup scripts to scripts directory.

---
+++ scripts/etc/install-winget.ps1
@@ -0,0 +1,19 @@
+Write-Information "Downloading WinGet and its dependencies..."
+
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile $PSScriptRoot/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+function Add-RemotePackage() {
+    param($uri)
+    # temp file to store the package in
+    $temp = New-TemporaryFile
+    # download to temp file
+    Invoke-WebRequest -Uri $uri -OutFile $temp -UseBasicParsing
+    # add package
+    Add-AppxPackage $temp
+    # remove package file
+    Remove-Item $temp
+}
+
+Add-RemotePackage -uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-RemotePackage -uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx
+Add-RemotePackage -uri https://aka.ms/getwinget
---
+++ scripts/setup-linux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# TODO: Check to make sure the distro is Ubuntu 18.04 (or whatever is currently supported)
+# TODO: Test this script on a fresh install of 18.04 (or whatever is currently supported)
+
+# Add source for git 
+sudo add-apt-repository ppa:git-core/ppa
+# Add source for unity hub
+wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+
+# Update apt-get 
+sudo apt-get update
+
+# Install the build-essential package
+sudo apt install build-essential -y
+
+# Install the latest version of Git (needed for things like git lfs)
+sudo apt install git
+
+# Install unity hub
+sudo apt-get install unityhub
+
+
+
