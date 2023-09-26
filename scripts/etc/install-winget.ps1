b9536d77ee71616b2367fbb91ffa98c62221a1b7
Paul Hazen <paul.hazen@playeveryware.com>
2023-09-25 18:08:04 -0700

    Updated winget installation script to delete install files after installation.

--- scripts/etc/install-winget.ps1
+++ scripts/etc/install-winget.ps1
@@ -4,6 +4,13 @@ Write-Information "Downloading WinGet and its dependencies..."
 Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
 Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+
+Write-Host "Installing packages for winget"
 Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
 Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+# Remove the downloaded files after they've been added.
+Remove-Item Microsoft.VCLibs.x64.14.00.Desktop.appx
+Remove-Item Microsoft.UI.Xaml.2.7.x64.appx
+Remove-Item Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
