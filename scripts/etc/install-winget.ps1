e4cb0dcdffc872038a33ee6a062edfeb2457b00e
Paul Hazen <paul.hazen@playeveryware.com>
2023-09-25 18:14:57 -0700

    trying to auto accept source agreements.

--- scripts/etc/install-winget.ps1
+++ scripts/etc/install-winget.ps1
@@ -6,9 +6,9 @@ Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -O
 Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
 
 Write-Host "Installing packages for winget"
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx -Confirm:$false --accept-source-agreements --accept-package-agreements
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx -Confirm:$false --accept-source-agreements --accept-package-agreements
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -Confirm:$false --accept-source-agreements --accept-package-agreements
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx --accept-source-agreements
+Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx --accept-source-agreements
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle --accept-source-agreements
 
 # Remove the downloaded files after they've been added.
 Remove-Item Microsoft.VCLibs.x64.14.00.Desktop.appx
