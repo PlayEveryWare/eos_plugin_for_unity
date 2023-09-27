3398b393df9fd6bd9d64cd217a6a4f1de399533d
Paul Hazen <paul.hazen@playeveryware.com>
2023-09-26 17:05:52 -0700

    Fixed editor version path stuff.

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# NOTE: The string below will need to be updated when the project is upgraded
+# to support newer versions of the editor
+UNITY_VERSION=2021.3.30f1
+
 # Add source for git 
 sudo add-apt-repository ppa:git-core/ppa -y
 
@@ -7,30 +11,23 @@ sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-get update
 
 # Install prereqs
-sudo dnf install openssl1.1 openssl-libs
+sudo dnf install openssl1.1 openssl-libs 
 
-# Install packages needed for development
-sudo apt install build-essential git -y
-
-# Install Unity 2021.3 (for some reason cannot specify 2021.3.8f1)
-# NOTE: The string below will need to be updated when the project is upgraded
-# to support newer versions of the editor
-# sudo unityhub --headless install --version 2021.3 --no-sandbox
-
+# Download and install the unity hub
 sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
 sudo apt update
-sudo apt install unityhub -y
+sudo apt install build-essential git unityhub -y
 sudo apt install libgbm-dev libasound2 libgconf-2-4 -y
 sudo apt install xvfb -y
 
-UNITY_VERSION=2021.3.30f1
-
+# Install Unity 2021.3 (for some reason cannot specify 2021.3.8f1)
 sudo xvfb-run unityhub --headless install --version $UNITY_VERSION --no-sandbox
 
 # Generate a license activation file (.alf)
-sudo xvfb-run Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity -quit -batchmode -nographics -createManualActivationFile
+sudo xvfb-run ~/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity -quit -batchmode -nographics -createManualActivationFile
 # Get the license
-sudo xvfb-run Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity -quit -batchmode -nographics -manualLicenseFile yourLicenseFile.ulf
+sudo xvfb-run ~/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity -quit -batchmode -nographics -manualLicenseFile yourLicenseFile.ulf
 
-sudo xvfb-run Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity -quit -batchmode -nographics -logFile - -projectPath ../ -buildTarget Linux64 -buildLinux64Player "Build.x86_64"
+# Try building the project
+sudo xvfb-run ~/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity -quit -batchmode -nographics -logFile - -projectPath ../ -buildTarget Linux64 -buildLinux64Player "Build.x86_64"
