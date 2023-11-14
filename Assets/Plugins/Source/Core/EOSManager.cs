d51ab3569e2a9f79bca7c842a7b2567ded486f3c
Giovanni Frigo <giovanni@belkadigital.com>
2023-11-14 17:54:51 +0100

    Fixed SimplePrintCallback breaking on some Language Cultures (i.e. Thai)

--- Assets/Plugins/Source/Core/EOSManager.cs
+++ Assets/Plugins/Source/Core/EOSManager.cs
@@ -691,7 +691,7 @@ namespace PlayEveryWare.EpicOnlineServices
                     type = LogType.Warning;
                 }
 
-                UnityEngine.Debug.LogFormat(type, LogOption.NoStacktrace, null, "{0:O} {1}({2}): {3}", dateTime, messageCategory, message.Level, message.Message);
+                UnityEngine.Debug.LogFormat(type, LogOption.NoStacktrace, null, "{0:O} {1}({2}): {3}", dateTime.ToString(DateTimeFormatInfo.InvariantInfo), messageCategory, message.Level, message.Message);
             }
 
             //-------------------------------------------------------------------------
