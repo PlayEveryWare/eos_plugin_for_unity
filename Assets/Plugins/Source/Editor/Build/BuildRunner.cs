a344e222016f132f7395b8655932109e16992aa9
Paul Hazen <paul-hazen@live.com>
2024-02-26 17:43:46 -0800

    wip: Started to implement platform-specific builders to be run by the BuildRunner.

---
+++ Assets/Plugins/Source/Editor/Build/BuildRunner.cs
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2023 PlayEveryWare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace PlayEveryWare.EpicOnlineServices.Build
+{
+    using Editor;
+    using System;
+    using System.IO;
+    using UnityEditor;
+    using UnityEditor.Build;
+    using UnityEditor.Build.Reporting;
+    using UnityEngine;
+    
+    public class BuildRunner : IPreprocessBuildWithReport, IPostprocessBuildWithReport
+    {
+        public int callbackOrder { get { return 0; } }
+
+        public static IPlatformSpecificBuilder GetBuilder()
+        {
+            IPlatformSpecificBuilder builder = null;
+            // NOTE: Try to make it so that (regarding build tasks) this is the ONLY place where compiler defines are
+            //       utilized. Reducing the number of places in the code where this happens will limit the number of
+            //       potential failure points, and keep the build process as linear as possible.
+
+            try
+            {
+#if UNITY_IOS
+                builder = new IOSBuilder();
+                PlatformManager.CurrentPlatform = PlatformManager.Platform.iOS;
+#elif UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX
+                builder = new LinuxBuilder();
+                PlatformManager.CurrentPlatform = PlatformManager.Platform.Linux;
+#elif UNITY_ANDROID
+                builder = new AndroidBuilder();
+                PlatformManager.CurrentPlatform = PlatformManager.Platform.Android;
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+                builder = new MacOSBuilder();
+                PlatformManager.CurrentPlatform = PlatformManager.Platform.macOS;
+#endif
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning($"Exception while creating builder: {e.Message}");
+            }
+
+            return builder;
+        }
+
+        /// <summary>
+        /// For EOS Plugin, this is the only place where anything can happen before build.
+        /// </summary>
+        /// <param name="report">The pre-process build report.</param>
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            // Do the various version-related tasks that need to be configured.
+            ConfigureVersions();
+
+            // perform the pre-build task for the platform using it's builder.
+            GetBuilder()?.PlatformPrebuild(report);
+        }
+
+        /// <summary>
+        /// For EOS Plugin, this is the only place where anything can happen as a post build task
+        /// </summary>
+        /// <param name="report">The report from the post-process build.</param>
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            // perform the post-build task for the platform using it's builder.
+            GetBuilder()?.PlatformPostbuild(report);
+
+            // Configure EAC
+            EACPostBuild.ConfigureEAC(report);
+        }
+
+        /// <summary>
+        /// Completes all configuration tasks.
+        /// </summary>
+        private static void ConfigureVersions()
+        {
+            AutoSetProductVersion();
+
+            const string packageVersionPath = "Assets/Resources/eosPluginVersion.asset";
+            string packageVersion = EOSPackageInfo.GetPackageVersion();
+            if (!AssetDatabase.IsValidFolder("Assets/Resources"))
+            {
+                AssetDatabase.CreateFolder("Assets", "Resources");
+            }
+            TextAsset versionAsset = new(packageVersion);
+            AssetDatabase.CreateAsset(versionAsset, packageVersionPath);
+            AssetDatabase.SaveAssets();
+        }
+
+        /// <summary>
+        /// Determines whether the Application Version is supposed to be used as the product version, and (if so) sets it accordingly.
+        /// </summary>
+        private static void AutoSetProductVersion()
+        {
+#if !EOS_DISABLE
+            var eosVersionConfigSection = new PrebuildConfigEditor();
+
+            eosVersionConfigSection.Load();
+
+            string configFilePath = Path.Combine(Application.streamingAssetsPath, "EOS", EOSPackageInfo.ConfigFileName);
+            var eosConfigFile = new ConfigHandler<EOSConfig>(configFilePath);
+            eosConfigFile.Read();
+
+            var previousProdVer = eosConfigFile.Data.productVersion;
+            var currentSectionConfig = eosVersionConfigSection.GetConfig().Data;
+
+            if (currentSectionConfig == null)
+            {
+                return;
+            }
+
+            if (currentSectionConfig.useAppVersionAsProductVersion)
+            {
+                eosConfigFile.Data.productVersion = Application.version;
+            }
+
+            if (previousProdVer != eosConfigFile.Data.productVersion)
+            {
+                eosConfigFile.Write(true);
+            }
+#endif
+        }
+    }
+}
---
+++ Assets/Plugins/Source/Editor/Build/IPlatformSpecificBuilder.cs
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024 PlayEveryWare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace PlayEveryWare.EpicOnlineServices.Build
+{
+    using UnityEditor.Build.Reporting;
+
+    /// <summary>
+    /// Interface for platform specific builders.
+    /// </summary>
+    public interface IPlatformSpecificBuilder
+    {
+        /// <summary>
+        /// Called by the BuildRunner before the build happens on the platform.
+        /// </summary>
+        /// <param name="report">The BuildReport for the prebuild task.</param>
+        public void PlatformPrebuild(BuildReport report);
+
+        /// <summary>
+        /// Called by the BuildRunner after the build happens on the platform.
+        /// </summary>
+        /// <param name="report">The BuildReport for the postbuild task.</param>
+        public void PlatformPostbuild(BuildReport report);
+    }
+}
