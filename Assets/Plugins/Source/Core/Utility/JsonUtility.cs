b53c12c7a4f43413dbcda47f57c04cc698744dd8
Paul Hazen <paul-hazen@live.com>
2024-03-29 11:37:17 -0700

    feat: Add methods to validate JSON, add comments to all functions in JsonUtility.

--- Assets/Plugins/Source/Core/Utility/JsonUtility.cs
+++ Assets/Plugins/Source/Core/Utility/JsonUtility.cs
@@ -22,27 +22,100 @@
 
 namespace PlayEveryWare.EpicOnlineServices.Utility
 {
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using System;
+    using UnityEngine;
+
     public static class JsonUtility
     {
+        /// <summary>
+        /// Attempts to parse the given JSON string. If problems are encountered, an error will be logged and the exception will continue.
+        /// </summary>
+        /// <param name="json">The JSON string to validate.</param>
+        private static void ValidateJson(string json)
+        {
+            try
+            {
+                JToken.Parse(json);
+            }
+            catch (JsonReaderException ex)
+            {
+                Debug.LogError($"Invalid JSON: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"An error occurred: {ex.Message}");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Determines if the given json string is valid json or not. If JSON is invalid, errors will be logged, but exceptions will be suppressed.
+        /// </summary>
+        /// <param name="json">The JSON string to validate.</param>
+        /// <returns>True if json is valid, false otherwise.</returns>
+        private static bool IsJsonValid(string json)
+        {
+            try
+            {
+                ValidateJson(json);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Convert an object into JSON.
+        /// </summary>
+        /// <param name="obj">The object to serialize into JSON.</param>
+        /// <param name="pretty">Whether to make the JSON pretty.</param>
+        /// <returns>String representation of the given object serialized.</returns>
         public static string ToJson(object obj, bool pretty = false)
         {
             return UnityEngine.JsonUtility.ToJson(obj, pretty);
         }
 
+        /// <summary>
+        /// Return an object deserialized from the given json file.
+        /// </summary>
+        /// <typeparam name="T">The type of object to deserialize.</typeparam>
+        /// <param name="filepath">The path to the JSON file.</param>
+        /// <returns>The deserialized object.</returns>
         public static T FromJsonFile<T>(string filepath)
         {
             string json = FileUtility.ReadAllText(filepath);
             return FromJson<T>(json);
         }
 
+        /// <summary>
+        /// Return an object deserialized from the given json string. If json is invalid, errors will
+        /// be logged, and an object with default values will be returned.
+        /// </summary>
+        /// <typeparam name="T">The type of object to deserialize.</typeparam>
+        /// <param name="json">The JSON string.</param>
+        /// <returns>The deserialized object.</returns>
         public static T FromJson<T>(string json)
         {
-            return UnityEngine.JsonUtility.FromJson<T>(json);
+            return (IsJsonValid(json)) ? UnityEngine.JsonUtility.FromJson<T>(json) : default;
         }
 
+        /// <summary>
+        /// Overwrites the given json object with values deserialized from the given json string.
+        /// If json is invalid, errors will be logged and no change will be made to the object.
+        /// </summary>
+        /// <param name="json">The string of json to deserialize values from.</param>
+        /// <param name="obj">The object to change the values of.</param>
         public static void FromJsonOverwrite(string json, object obj)
         {
-            UnityEngine.JsonUtility.FromJsonOverwrite(json, obj);
+            if (IsJsonValid(json))
+            {
+                UnityEngine.JsonUtility.FromJsonOverwrite(json, obj);
+            }
         }
     }
 }    
