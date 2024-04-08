f0e4427927d07ce1ed46caf6c5d4e146dd135615
arthur272 <arthur740212@gmail.com>
2024-04-08 14:58:30 -0700

    fix(steam,editor) : reintroduce asmdef for steamworks version check button

--- Assets/Plugins/Source/Editor/Utility/Steam/Steamworks_Utility.cs
+++ Assets/Plugins/Source/Editor/Utility/Steam/Steamworks_Utility.cs
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2024 PlayEveryWare
+* Copyright (c) 2021 PlayEveryWare
 * 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
