c4302cc2dbf54d4f8438ff8b9c34772ca5ef9de1
Paul Hazen <paul-hazen@live.com>
2024-04-12 14:34:54 -0700

    Revert "chore: Remove all unreferenced prefabs in project. Clean UnityProjectUtility class implementation."
    This reverts commit b3fee5998064c49b4fee6653e0bc5572d15b9f69.
    
    fix: Return errantly removed prefab files.

--- Assets/Plugins/Source/Editor/Utility/UnityProjectUtility.cs
+++ Assets/Plugins/Source/Editor/Utility/UnityProjectUtility.cs
@@ -20,8 +20,7 @@
  * SOFTWARE.
  */
 
-// NOTE: Uncomment the following define to enable menu items in the Unity Editor
-//       that are implemented below.
+// Uncomment the following define to enable menu items detailed below
 // #define PROJECT_UTILITY_ENABLED
 
 #if PROJECT_UTILITY_ENABLED
@@ -34,78 +33,9 @@ namespace PlayEveryWare.EpicOnlineServices.Editor.Utility
     using UnityEditor.SceneManagement;
     using UnityEngine.SceneManagement;
     using System.Collections.Generic;
-    using System.Linq;
 
-    #region Extension Methods
-
-    /*
-     * NOTE: In the typical course of a project, a single class per file policy is good to
-     *       adhere to, however in context with the scope of the project as one that is
-     *       not centered on extending Unity, the decision was made to keep these within the
-     *       current file. If these focuses or scopes change, it might make sense to break
-     *       these extension classes off.
-     */
-
-    /// <summary>
-    /// Handles a variety of extensions to GameObjects to make it easier to follow the logic of
-    /// how they are being inspected.
-    /// </summary>
-    public static class GameObjectExtensions
-    {
-        /// <summary>
-        /// Retrieves a unique list of GUIDs for each prefab that is attached as a component to the given GameObject.
-        /// </summary>
-        /// <param name="gameObject">The GameObject to find all the prefab GUIDs of.</param>
-        /// <returns>A unique set of GUIDs for each prefab attached to given GameObject.</returns>
-        public static HashSet<string> GetPrefabGuids(this GameObject gameObject)
-        {
-            HashSet<string> guids = new();
-            var components = gameObject.GetComponents<Component>();
-            foreach (var component in components)
-            {
-                if (component == null) continue;
-
-                SerializedObject so = new(component);
-                SerializedProperty sp = so.GetIterator();
-
-                while (sp.Next(true))
-                {
-                    // If the SerializedProperty is either not an ObjectReference, or if the value of that reference is null, then continue
-                    if (sp.propertyType != SerializedPropertyType.ObjectReference || sp.objectReferenceValue == null)
-                    {
-                        continue;
-                    }
-
-                    string path = AssetDatabase.GetAssetPath(sp.objectReferenceValue);
-
-                    // Skip if the path is null, or if it is not a prefab.
-                    if (string.IsNullOrEmpty(path) || !path.EndsWith(".prefab")) continue;
-
-                    string guid = AssetDatabase.AssetPathToGUID(path);
-                    guids.Add(guid);
-                }
-            }
-
-            return guids;
-        }
-
-        public static string GetFullPath(this GameObject gameObject)
-        {
-            return gameObject.transform.parent == null
-                ? gameObject.name
-                : gameObject.transform.parent.gameObject.GetFullPath() + "/" + gameObject.name;
-        }
-    }
-    
-    #endregion
-
-    /// <summary>
-    /// Provides utility functions for inspecting a Unity project.
-    /// </summary>
     public class UnityProjectUtility
     {
-        #region Methods to find missing scripts
-
         [MenuItem("Tools/Find Missing Scripts/In Prefabs")]
         public static void FindAllMissingScriptsInPrefabs()
         {
@@ -114,30 +44,42 @@ namespace PlayEveryWare.EpicOnlineServices.Editor.Utility
 
             foreach (string assetPath in allAssetPaths)
             {
-                // If the asset isn't a prefab, then continue
-                if (!assetPath.EndsWith(".prefab")) { continue; }
-
-                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
-                Component[] components = prefab.GetComponentsInChildren<Component>(true);
-                
-                // If none of the components are null (if they are all not null) then continue
-                if (components.All(component => component != null)) { continue; }
-
-                Debug.LogError("Missing script found in prefab: " + assetPath, prefab);
-                missingScriptCount++;
+                if (assetPath.EndsWith(".prefab"))
+                {
+                    GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+                    Component[] components = prefab.GetComponentsInChildren<Component>(true);
+                    foreach (Component component in components)
+                    {
+                        if (component == null)
+                        {
+                            Debug.LogError("Missing script found in prefab: " + assetPath, prefab);
+                            missingScriptCount++;
+                            break; // Once a missing script is found, no need to check further.
+                        }
+                    }
+                }
             }
 
-            Debug.Log(missingScriptCount == 0
-                ? "No missing scripts found in any prefabs."
-                : $"{missingScriptCount} prefabs with missing scripts found.");
+            if (missingScriptCount == 0)
+            {
+                Debug.Log("No missing scripts found in any prefabs.");
+            }
+            else
+            {
+                Debug.LogFormat("{0} prefabs with missing scripts found.", missingScriptCount);
+            }
+        }
+
+        [MenuItem("Tools/Find Missing Scripts/In Prefabs", isValidateFunction: true)]
+        public static bool CanFindMissingScriptsInPrefabs()
+        {
+            return !EditorApplication.isPlaying;
         }
 
         [MenuItem("Tools/Find Missing Scripts/In Scenes")]
-        private static void FindMissingScriptsInScenes()
+        private static void FindMissingScripts()
         {
-            List< (string sceneName, string goScriptIsAttachedTo)> missingScripts = new();
-
-            // Store the currently open scene, so that it can be opened when the operation is complete.
+            // Store the currently open scene
             var previouslyOpenScene = SceneManager.GetActiveScene().path;
 
             // Iterate over all the scenes
@@ -158,36 +100,24 @@ namespace PlayEveryWare.EpicOnlineServices.Editor.Utility
                     {
                         // Skip if the component is there.
                         if (component != null) continue;
-                        
-                        // Record details of the missing script.
-                        missingScripts.Add((go.GetFullPath(), Path.GetFileNameWithoutExtension(scenePath)));
+
+                        Debug.LogError(
+                            $"Script \"{FullGameObjectPath(go)}\" in scene \"{Path.GetFileNameWithoutExtension(scenePath)}\" is missing.",
+                            go);
                     }
                 }
-            }
-
-            // Log the details of each missing script.
-            foreach ((string sceneName, string goScriptIsAttachedTo) in missingScripts)
-            {
-                Debug.LogError($"Script is missing from GameObject \"{goScriptIsAttachedTo}\" in scene \"{sceneName}\".");
             }
 
             // Reopen the scene that was open originally
             EditorSceneManager.OpenScene(previouslyOpenScene);
         }
 
-        #endregion
-
-        [MenuItem("Tools/Find Unreferenced Prefabs",       isValidateFunction: true)]
-        [MenuItem("Tools/Find Missing Scripts/In Scenes",  isValidateFunction: true)]
-        [MenuItem("Tools/Find Missing Scripts/In Prefabs", isValidateFunction: true)]
-        public static bool IsNotInPlayMode()
+        [MenuItem("Tools/Find Missing Scripts/In Scenes", isValidateFunction: true)]
+        private static bool CanFindMissingScriptsInScenes()
         {
-            // TODO: The attribute "MenuItem" has a parameter for defining the EditorModes in which it should be enabled
-            //       making use of that parameter would be a much cleaner solution.
+            // Scenes can only be searched if not in play mode.
             return !EditorApplication.isPlaying;
         }
-
-        #region Methods to find unreferenced prefabs
 
         [MenuItem("Tools/Find Unreferenced Prefabs")]
         public static void FindAllUnreferencedPrefabs()
@@ -200,53 +130,71 @@ namespace PlayEveryWare.EpicOnlineServices.Editor.Utility
                 return;
             }
 
-            HashSet<string> referencedPrefabGuids = new();
-            HashSet<string> checkedPrefabGuids = new();
+            HashSet<string> s_referencedPrefabGuids = new();
+            HashSet<string> s_checkedPrefabGuids = new();
 
             // First pass: Find prefabs referenced in scenes
             foreach (var scenePath in EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes))
             {
                 var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Additive);
-                
-                GetPrefabGuidsInCurrentScene(ref referencedPrefabGuids);
+                FindPrefabsInCurrentScene(ref s_referencedPrefabGuids);
             }
 
-            Debug.Log($"There are a total of {referencedPrefabGuids.Count} prefabs directly referenced by scenes.");
+            Debug.Log($"There are a total of {s_referencedPrefabGuids.Count} prefabs directly referenced by scenes.");
 
             // Second pass: Find prefabs referenced by other prefabs
             Debug.Log($"Now checking each prefab for prefab dependencies.");
-            var prefabsReferencedByScenesDirectory = new HashSet<string>(referencedPrefabGuids);
+            var prefabsReferencedByScenesDirectory = new HashSet<string>(s_referencedPrefabGuids);
             foreach (var prefabGuid in prefabsReferencedByScenesDirectory)
             {
-                FindPrefabDependencies(ref checkedPrefabGuids, ref referencedPrefabGuids, prefabGuid);
+                FindPrefabDependencies(ref s_checkedPrefabGuids, ref s_referencedPrefabGuids, prefabGuid);
             }
 
             // Compare to list of all prefabs
             string[] allPrefabGuids = AssetDatabase.FindAssets("t:Prefab");
-            Debug.Log($"There were a total of {allPrefabGuids.Length - referencedPrefabGuids.Count} unreferenced prefabs.");
+            Debug.Log(
+                $"Comparing discovered (referenced) prefabs ({s_referencedPrefabGuids.Count}) to all prefabs ({allPrefabGuids.Length}).");
             foreach (var prefabGuid in allPrefabGuids)
             {
-                // If the prefab has been referenced, then continue
-                if (referencedPrefabGuids.Contains(prefabGuid)) { continue; }
-
-                string path = AssetDatabase.GUIDToAssetPath(prefabGuid);
-                Debug.Log("Unreferenced Prefab: " + path);
+                if (!s_referencedPrefabGuids.Contains(prefabGuid))
+                {
+                    string path = AssetDatabase.GUIDToAssetPath(prefabGuid);
+                    Debug.Log("Unreferenced Prefab: " + path);
+                }
             }
         }
 
-        /// <summary>
-        /// Adds the GUIDs of all prefabs in the current scene to the given HashSet of Guids
-        /// </summary>
-        /// <param name="guids">The HashSet to add the Guids to.</param>
-        private static void GetPrefabGuidsInCurrentScene(ref HashSet<string> guids)
+        private static void FindPrefabsInCurrentScene(ref HashSet<string> referencedPrefabGuids)
         {
-            GameObject[] allGameObjects = Object.FindObjectsOfType<GameObject>(true);
+            GameObject[] allGameObjects = GameObject.FindObjectsOfType<GameObject>(true);
             foreach (var go in allGameObjects)
             {
-                var prefabGuidsOnGameObject = go.GetPrefabGuids();
-                foreach (var guid in prefabGuidsOnGameObject)
+                AddPrefabReferencesFromGameObject(ref referencedPrefabGuids, go);
+            }
+        }
+
+        private static void AddPrefabReferencesFromGameObject(ref HashSet<string> referencedPrefabGuids, GameObject go)
+        {
+            var components = go.GetComponents<Component>();
+            foreach (var component in components)
+            {
+                if (component == null) continue;
+
+                SerializedObject so = new SerializedObject(component);
+                SerializedProperty sp = so.GetIterator();
+
+                while (sp.Next(true))
                 {
-                    guids.Add(guid);
+                    if (sp.propertyType == SerializedPropertyType.ObjectReference && sp.objectReferenceValue != null)
+                    {
+                        string path = AssetDatabase.GetAssetPath(sp.objectReferenceValue);
+
+                        // Skip if the path is null, or if it is not a prefab.
+                        if (string.IsNullOrEmpty(path) || !path.EndsWith(".prefab")) continue;
+
+                        string guid = AssetDatabase.AssetPathToGUID(path);
+                        referencedPrefabGuids.Add(guid);
+                    }
                 }
             }
         }
@@ -266,12 +214,8 @@ namespace PlayEveryWare.EpicOnlineServices.Editor.Utility
                 return;
             }
 
-            var prefabsOnPrefabs = prefab.GetPrefabGuids();
-            foreach (var prefabChild in prefabsOnPrefabs)
-            {
-                referencedPrefabGuids.Add(prefabChild);
-            }
-            
+            AddPrefabReferencesFromGameObject(ref referencedPrefabGuids, prefab);
+
             // Check if this prefab references other prefabs and add them to the list
             Component[] components = prefab.GetComponentsInChildren<Component>(true);
             foreach (Component component in components)
@@ -279,7 +223,7 @@ namespace PlayEveryWare.EpicOnlineServices.Editor.Utility
                 // skip if the component is null
                 if (null == component) continue;
 
-                SerializedObject so = new(component);
+                SerializedObject so = new SerializedObject(component);
                 SerializedProperty sp = so.GetIterator();
                 while (sp.Next(true))
                 {
@@ -292,18 +236,34 @@ namespace PlayEveryWare.EpicOnlineServices.Editor.Utility
                     if (string.IsNullOrEmpty(depPath) || !depPath.EndsWith(".prefab")) continue;
 
                     string depGuid = AssetDatabase.AssetPathToGUID(depPath);
-
-                    // If the prefab has been referenced, then continue
-                    if (referencedPrefabGuids.Contains(depGuid)) { continue; }
-
-                    referencedPrefabGuids.Add(depGuid);
-                    FindPrefabDependencies(ref checkedPrefabGuids, ref referencedPrefabGuids,
-                        depGuid); // Recursively check this new prefab
+                    if (!referencedPrefabGuids.Contains(depGuid))
+                    {
+                        referencedPrefabGuids.Add(depGuid);
+                        FindPrefabDependencies(ref checkedPrefabGuids, ref referencedPrefabGuids,
+                            depGuid); // Recursively check this new prefab
+                    }
                 }
             }
         }
 
-        #endregion
+        [MenuItem("Tools/Find Unreferenced Prefabs", isValidateFunction: true)]
+        public static bool CanFindUnreferencedPrefabs()
+        {
+            return !EditorApplication.isPlaying;
+        }
+
+        /// <summary>
+        /// Determine the full path of a given GameObject recursively.
+        /// </summary>
+        /// <param name="go">The game object to obtain the path of.</param>
+        /// <returns>A string path indicating the path of the GameObject.</returns>
+        private static string FullGameObjectPath(GameObject go)
+        {
+            return go.transform.parent == null
+                ? go.name
+                : FullGameObjectPath(go.transform.parent.gameObject) + "/" + go.name;
+        }
     }
+
 }
 #endif
