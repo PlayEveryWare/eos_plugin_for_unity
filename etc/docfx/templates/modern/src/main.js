1211a7396dad14e05a0f797b104b4b88e6c8afbf
Paul Hazen <paul-hazen@live.com>
2024-04-15 10:09:04 -0700

    doc: Update docfx to use modern theme, and set navbar icon to the PEW logo.

---
+++ etc/docfx/templates/modern/src/main.js
@@ -0,0 +1,14 @@
+/**
+ * Licensed to the .NET Foundation under one or more agreements.
+ * The .NET Foundation licenses this file to you under the MIT license.
+ */
+
+export default {
+    iconlinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/PlayEveryWare/eos_plugin_for_unity',
+            title: 'GitHub'
+        }
+    ]
+}
