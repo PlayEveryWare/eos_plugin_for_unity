5ce1f96ed65f05dd52bca726a7302b39a64fc03e
Paul Hazen <paul-hazen@live.com>
2024-05-03 11:26:30 -0700

    fix: Change name of AndroidPlatformSpecifics class to match the file it's contained in and the naming convention applied to other platforms.

--- Assets/Plugins/Android/Core/AndroidPlatformSpecifics.cs
+++ Assets/Plugins/Android/Core/AndroidPlatformSpecifics.cs
@@ -48,18 +48,18 @@ namespace PlayEveryWare.EpicOnlineServices
 
     //-------------------------------------------------------------------------
     // Android specific Unity Parts.
-    public class EOSPlatformSpecificsAndroid : PlatformSpecifics<AndroidConfig>
+    public class AndroidPlatformSpecifics : PlatformSpecifics<AndroidConfig>
     {
 
         [DllImport("UnityHelpers_Android")]
         private static extern JavaVM UnityHelpers_GetJavaVM();
 
-        public EOSPlatformSpecificsAndroid() : base(PlatformManager.Platform.Android, ".so") { }
+        public AndroidPlatformSpecifics() : base(PlatformManager.Platform.Android, ".so") { }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         static public void Register()
         {
-            EOSManagerPlatformSpecificsSingleton.SetEOSManagerPlatformSpecificsInterface(new EOSPlatformSpecificsAndroid());
+            EOSManagerPlatformSpecificsSingleton.SetEOSManagerPlatformSpecificsInterface(new AndroidPlatformSpecifics());
         }
 
         private static void ConfigureAndroidActivity()
