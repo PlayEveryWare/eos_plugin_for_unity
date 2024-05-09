7a3a2b09cae4cffb70e114a118db30bf68c44ad8
Paul Hazen <paul-hazen@live.com>
2024-05-08 22:55:44 -0700

    test: Add unit test to confirm functionality of type to factory method registration for Config classes.

--- Assets/Tests/PlayMode/ConfigTests.cs
+++ Assets/Tests/PlayMode/ConfigTests.cs
@@ -8,8 +8,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -22,27 +22,89 @@
 
 using NUnit.Framework;
 
-namespace PlayEveryWare.EpicOnlineServices.Editor.Utility
+namespace PlayEveryWare.EpicOnlineServices.Tests
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
     public class ConfigTests
     {
         /// <summary>
-        /// Tests that when you try to convert a negative int to a uint, the conversion
-        /// fails, and the output value is set to a straightforward cast.
+        /// Finds all non-abstract derived classes of a specified type within
+        /// the same assembly. Eventually, this particular component can be put
+        /// into a class containing generic reflection utility functions,
+        /// because currently ConfigTests is the only place this utility method
+        /// is used, it is kept within the ConfigTests class until it's needed
+        /// elsewhere.
         /// </summary>
-        [Test]
-        public static void TryConvert_IntToUint_NegativeInt_ReturnsFalse()
+        /// <typeparam name="TBase">
+        /// The type to find derived classes for.
+        /// </typeparam>
+        /// <returns>
+        /// An enumerable of types that are non-abstract and derived from the
+        /// specified base type.
+        /// </returns>
+        private static IEnumerable<Type> FindDerivedTypes<TBase>()
+            where TBase : class
         {
-            PlayEveryWare.EpicOnlineServices.Config testConfig;
+            // Get the assembly containing the type.
+            Assembly assembly = Assembly.GetAssembly(typeof(TBase));
 
-           
+            // Find all types in the assembly that are subclass of TBase and are
+            // not abstract.
+            var derivedTypes = assembly.GetTypes()
+                .Where(t => t.IsClass &&
+                            !t.IsAbstract &&
+                            t.IsSubclassOf(typeof(TBase)));
+
+            return derivedTypes;
         }
 
+        /// <summary>
+        /// Due to the way that the Config base class requires registration of
+        /// the config type with the constructor for that type in-order to
+        /// accomplish the factory pattern, it is necessary to check that the
+        /// type requested equals the type that the factory method generates.
+        /// </summary>
+        [Test]
         public static void Get_ConfigDerivedClass_Valid()
         {
-            PlayEveryWare.EpicOnlineServices.Config test;
 
-            var configTypes = ReflectionUtility.CreateInstancesOfDerivedGenericClasses(typeof(PlayEveryWare.EpicOnlineServices.))
+            // Setup by forcing the static constructors to execute for the known
+            // classes that derive from Config.
+            var derivedTypes = FindDerivedTypes<Config>();
+
+            // Iterate through the types that derive from the base abstract
+            // Config class.
+            foreach (var type in derivedTypes)
+            {
+                // Make sure that the static constructor is called before its
+                // factory method is tested.
+                ConstructorInfo staticConstructor = type.TypeInitializer;
+                staticConstructor?.Invoke(null, null);
+
+                // Get the "generic" version of the get method (factory method
+                // used to retrieve a Config of a certain type.
+                var getMethod = typeof(Config).GetMethod("Get");
+
+                // Do not continue if the factory method could not be created.
+                if (null == getMethod)
+                    continue;
+
+                // Create the typed version of the factory method.
+                var typedGetMethod = getMethod.MakeGenericMethod(type);
+
+                // Obtain a config object using the factory method.
+                var config = typedGetMethod.Invoke(
+                    null, 
+                    null);
+
+                // Make certain that the factory method generates the expected
+                // type.
+                Assert.AreEqual(config.GetType(), type);
+            }
         }
     }
 }
