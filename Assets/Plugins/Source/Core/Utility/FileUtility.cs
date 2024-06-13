01a896da3a606624a7b06058f54dc90fc9938fc0
Paul Hazen <paul-hazen@live.com>
2024-06-12 18:14:39 -0700

    chore: Revert change made to FileUtility that should be in a different PR.

--- Assets/Plugins/Source/Core/Utility/FileUtility.cs
+++ Assets/Plugins/Source/Core/Utility/FileUtility.cs
@@ -393,7 +393,12 @@ namespace PlayEveryWare.EpicOnlineServices.Utility
                     // Only retry if there are remaining attempts.
                     await Task.Delay(delay, cancellationToken);
                 }
+
             }
+
+            // Run the file copy asynchronously, passing on the
+            // cancellation token.
+            await Task.Run(() => File.Copy(op.SourcePath, op.DestinationPath, true), cancellationToken);
         }
 
         /// <summary>
