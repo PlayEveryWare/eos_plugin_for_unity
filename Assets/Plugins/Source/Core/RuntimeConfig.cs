4e9ba1f8dae68a14e69e52fb277f841baa485f48
Paul Hazen <paul-hazen@live.com>
2024-07-24 14:38:00 -0700

    Make the RuntimeConfig a readonly struct.

--- Assets/Plugins/Source/Core/RuntimeConfig.cs
+++ Assets/Plugins/Source/Core/RuntimeConfig.cs
@@ -47,7 +47,7 @@ namespace PlayEveryWare.EpicOnlineServices
     /// Most of the values for these field members come from the
     /// [Development Portal](https://dev.epicgames.com/portal/)
     /// </summary>
-    public struct RuntimeConfig
+    public readonly struct RuntimeConfig
     {
 
         /*
