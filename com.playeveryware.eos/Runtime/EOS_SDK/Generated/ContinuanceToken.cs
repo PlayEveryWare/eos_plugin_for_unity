3c84b85d2892087a97f4a5a9cf69c9b2eac6e6b9
arthur272 <arthur740212@gmail.com>
2024-08-22 12:37:39 -0700

    chore(sdk,source) : updated managed source for 1.16.3 hotfix
    Including PEW edit for continuance token

--- com.playeveryware.eos/Runtime/EOS_SDK/Generated/ContinuanceToken.cs
+++ com.playeveryware.eos/Runtime/EOS_SDK/Generated/ContinuanceToken.cs
@@ -50,9 +50,10 @@ namespace Epic.OnlineServices
 			// Call the EOS_ContinuanceToken_ToString function will a null buffer address to determine the correct buffer length.
 			// Discard the result of this call because it is not relevant. 
 			_ = Bindings.EOS_ContinuanceToken_ToString(InnerHandle, System.IntPtr.Zero, ref inOutBufferLength);
-			// PEW: End modify
-			
-			var funcResult = Bindings.EOS_ContinuanceToken_ToString(InnerHandle, outBufferAddress, ref inOutBufferLength);
+            outBufferAddress = Helper.AddAllocation(inOutBufferLength);
+            // PEW: End modify
+
+            var funcResult = Bindings.EOS_ContinuanceToken_ToString(InnerHandle, outBufferAddress, ref inOutBufferLength);
 
 			Helper.Get(outBufferAddress, out outBuffer);
 			Helper.Dispose(ref outBufferAddress);
