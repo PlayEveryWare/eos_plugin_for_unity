1a2fb9374aaf9c0c0137c73d444d82d45f720ed8
Paul Hazen <paul-hazen@live.com>
2024-08-28 10:21:18 -0700

    fix: Remove unneeded using statement.

--- com.playeveryware.eos/Runtime/Core/Utility/FileSystemUtility.cs
+++ com.playeveryware.eos/Runtime/Core/Utility/FileSystemUtility.cs
@@ -29,7 +29,6 @@ namespace PlayEveryWare.EpicOnlineServices.Utility
     using System;
     using System.Collections.Generic;
     using System.IO;
-    using System.Runtime.CompilerServices;
     using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
