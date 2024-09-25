2c6d3e5bd4bcb15947330f81694007501ab8ca7d
Paul Hazen <paul-hazen@live.com>
2024-09-25 14:06:44 -0700

    feat: Change implementation from using JsonUtility to using Newtonsoft.

--- com.playeveryware.eos/Runtime/Core/Utility/JsonUtility.cs
+++ com.playeveryware.eos/Runtime/Core/Utility/JsonUtility.cs
@@ -23,68 +23,70 @@
 namespace PlayEveryWare.EpicOnlineServices.Utility
 {
     using UnityEngine;
+    using Newtonsoft.Json;
+    using System;
 
     /// <summary>
-    /// Contains functions to interact with various json data.
+    /// Contains functions to interact with various JSON data.
     /// </summary>
     public static class JsonUtility
     {
         /// <summary>
         /// Tries to parse the given JSON into an object.
         /// </summary>
-        /// <typeparam name="T">The type to deserialize from the given json.
+        /// <typeparam name="T">
+        /// The type to deserialize from the given JSON.
         /// </typeparam>
         /// <param name="json">
         /// The JSON to deserialize from.
         /// </param>
-        /// <param name="obj">The object to contain the deserialized value.
+        /// <param name="obj">
+        /// The object to contain the deserialized value.
         /// </param>
         /// <returns>
-        /// True if the json was successfully deserialized, false otherwise.
+        /// True if the JSON was successfully deserialized, false otherwise.
         /// </returns>
         private static bool TryFromJson<T>(string json, out T obj)
         {
             obj = default;
             try
             {
-                obj = UnityEngine.JsonUtility.FromJson<T>(json);
+                obj = JsonConvert.DeserializeObject<T>(json);
                 return true;
             }
-            catch
+            catch (Exception ex)
             {
                 Debug.LogError($"Unable to parse object of type " +
                                $"\"{typeof(T).FullName}\" from " +
-                               $"JSON: \"{json}\".");
+                               $"JSON: \"{json}\". Exception: {ex.Message}");
 #if UNITY_EDITOR
-                // If running in the context of the Unity editor, then throw the
-                // exception in an effort to "fail loudly" so that the user is
-                // more likely to fix the problem. If not in editor mode, still
-                // log the error, but do not throw an exception.
                 throw;
 #else
-                // The else conditional is here because if an exception is not
-                // thrown, a value still needs to be returned.
                 return false;
 #endif
             }
         }
 
         /// <summary>
-        /// Convert an object into JSON.
+        /// Converts an object into JSON.
         /// </summary>
-        /// <param name="obj">The object to serialize into JSON.</param>
-        /// <param name="pretty">Whether to make the JSON pretty.</param>
+        /// <param name="obj">
+        /// The object to serialize into JSON.
+        /// </param>
+        /// <param name="pretty">
+        /// Whether to make the JSON pretty.
+        /// </param>
         /// <returns>
         /// String representation of the given object serialized.
         /// </returns>
         public static string ToJson(object obj, bool pretty = false)
         {
-            return UnityEngine.JsonUtility.ToJson(obj, pretty);
+            return JsonConvert.SerializeObject(obj, pretty ? Formatting.Indented : Formatting.None);
         }
 
         /// <summary>
-        /// Return an object deserialized from the given json string. If json is
-        /// invalid, errors will be logged, and an object with default values
+        /// Returns an object deserialized from the given JSON string. If JSON 
+        /// is invalid, errors will be logged, and an object with default values
         /// will be returned depending on whether running in editor mode or as a
         /// standalone.
         /// </summary>
@@ -93,17 +95,12 @@ namespace PlayEveryWare.EpicOnlineServices.Utility
         /// <returns>The deserialized object.</returns>
         public static T FromJson<T>(string json)
         {
-            // NOTE: The return value of the TryFromJson method call below is
-            //       not observed, because regardless of if the from json method
-            //       was successful, the value defined by the out variable is
-            //       returned to the caller.
             _ = TryFromJson(json, out T returnObject);
-
             return returnObject;
         }
 
         /// <summary>
-        /// Return an object deserialized from the given json file.
+        /// Returns an object deserialized from the given JSON file.
         /// </summary>
         /// <typeparam name="T">The type of object to deserialize.</typeparam>
         /// <param name="filepath">The path to the JSON file.</param>
@@ -115,39 +112,37 @@ namespace PlayEveryWare.EpicOnlineServices.Utility
         }
 
         /// <summary>
-        /// Overwrites the given json object with values deserialized from the
-        /// given json string. If json is invalid, errors will be logged and no
-        /// change will be made to the object.
+        /// Overwrites the given object's properties with values deserialized 
+        /// from the given JSON string. If JSON is invalid, errors will be 
+        /// logged and no change will be made to the object.
         /// </summary>
+        /// <typeparam name="T">
+        /// The type of the object to populate.
+        /// </typeparam>
         /// <param name="json">
-        /// The string of json to deserialize values from.
+        /// The JSON string to deserialize values from.
         /// </param>
         /// <param name="obj">
         /// The object to change the values of.
         /// </param>
         public static void FromJsonOverwrite<T>(string json, T obj)
         {
-            // NOTES:
-            //
-            // If the type 'T' indicated is abstract, then it is not possible to
-            // use UnityEngine.JsonUtility to verify the validity of the
-            // incoming JSON. This is because the validation is accomplished by
-            // performing the deserialization step and catching any thrown
-            // exceptions. Therefore, if the type indicated is abstract, don't
-            // bother trying to validate, go straight to using the
-            // 'FromJsonOverwrite' function in UnityEngine.JsonUtility.
-            //
-            // If the type indicated is _not_ abstract, then it is possible to
-            // utilize the validation approach, therefore in that scenario
-            // perform validation before performing the intended operation.
-            //
-            // That this code can only validate JSON representing a non-abstract
-            // class is a limitation of the UnityEngine.JsonUtility. Most other
-            // available libraries will support this and remove the need for
-            // this check.
-            if (typeof(T).IsAbstract || TryFromJson(json, out T _))
+            if (obj == null)
             {
-                UnityEngine.JsonUtility.FromJsonOverwrite(json, obj);
+                throw new ArgumentNullException(nameof(obj));
+            }
+            try
+            {
+                JsonConvert.PopulateObject(json, obj);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Unable to populate object of type " +
+                               $"\"{typeof(T).FullName}\" from " +
+                               $"JSON: \"{json}\". Exception: {ex.Message}");
+#if UNITY_EDITOR
+                throw;
+#endif
             }
         }
     }
