04a4349926dfbe522781297204f316ce36f80a5a
Paul Hazen <paul-hazen@live.com>
2024-10-10 10:29:40 -0700

    wip: [UNSTABLE] Add driver application to demonstrate functionality of enum parsing.

--- lib/NativeCode/DynamicLibraryLoaderHelper/Driver/Driver.cpp
+++ lib/NativeCode/DynamicLibraryLoaderHelper/Driver/Driver.cpp
@@ -500,16 +500,4 @@ int main()
     auto eos_config_as_json = read_config_json_as_json_from_path("C:\\Users\\PaulPEW\\dev\\repos\\eos_plugin_for_unity\\Assets\\StreamingAssets\\EOS\\EpicOnlineServicesConfig.json");
     auto eos_config = eos_config_from_json_value(eos_config_as_json);
     std::cout << "Hello World!\n";
-    
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
